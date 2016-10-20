b22ba325d24215364f304a27f376a7e6990bc2f9
Carl Eiserman <carleiserman@gmail.com>
2016-10-20 10:27:09 +0200

    Pre-Release: Scraped OpenFn URL

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,6 @@ angular.module('app.controllers', [])
 
 // Constants
 .constant('OPENFN_URL', "http://requestb.in/x6qg17x6")
-// .constant('OPENFN_URL', "URLHERE")
 //TODO: Add URL for OpenFunction #CARL
 
 .constant('SMS_TIMEOUT_PERIOD', 30)   //seconds
