0b8a803c16f32a9a1ad9c124397a7984614bc2a8
Carl Eiserman <carleiserman@gmail.com>
2016-11-16 10:58:43 +0200

    Removed secrets from controllers file

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,4 @@
 angular.module('app.controllers', [])
 
 // Constants
-// .constant('OPENFN_URL', "http://requestb.in/rfjmg9rf")
-  .constant('OPENFN_URL', "URLHERE")
-  //TODO: Add URL for OpenFunction #CARL
-
 .constant('SMS_TIMEOUT_PERIOD', 30); //seconds
