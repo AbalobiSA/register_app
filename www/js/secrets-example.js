aaf7836181d6df13cc4205c5d35b22c8a62ef523
Carl Eiserman <carleiserman@gmail.com>
2016-11-16 11:51:56 +0200

    Added example secrets file with instructions

---
+++ www/js/secrets-example.js
@@ -0,0 +1,11 @@
+/*
+    Registration Secrets file
+    =========================
+
+    This is a simple untracked extension on the controller.
+    1. Copy this file to a new file, secrets.js
+    2. Replace the 'YOUR_URL_HERE' with the desired URL.
+
+*/
+angular.module('app.controllers')
+  .constant('OPENFN_URL', "YOUR_URL_HERE");
