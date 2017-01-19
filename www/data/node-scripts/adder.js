3a1978c7b7b815b81f498e541b666048a37dca1d
Carl Eiserman <carleiserman@gmail.com>
2017-01-19 13:42:33 +0200

    Completed variable adder script

--- www/data/node-scripts/adder.js
+++ www/data/node-scripts/adder.js
@@ -6,4 +6,18 @@ console.log("==============================================");
 var english = JSON.parse(fs.readFileSync("../locale-en.json"));
 var afrikaans = JSON.parse(fs.readFileSync("../locale-afr.json"));
 
-var newVariables = ["HELLO", "HELLO2", "HELLO3"];
+var newVariables = [
+  "HELLO",
+  "HELLO2",
+  "HELLO3"
+];
+
+for (var i = 0; i < newVariables.length; i++){
+    english[newVariables[i]] = "CHANGE_MY_TEXT_PLEASE";
+    afrikaans[newVariables[i]] = "CHANGE_MY_TEXT_PLEASE";
+}
+
+console.log("Writing files...");
+fs.writeFileSync("../locale-en.json", JSON.stringify(english, null, 4));
+fs.writeFileSync("../locale-afr.json", JSON.stringify(english, null, 4));
+console.log("Success!");
